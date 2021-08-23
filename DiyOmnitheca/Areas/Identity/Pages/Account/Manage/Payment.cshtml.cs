b04e8ec3c891af5dc4a7e4b5ff7c5f07bddd0ef2
G.Zlatev <58149328+g-zlatev@users.noreply.github.com>
2021-08-23 23:18:49 +0300

    account management changes

--- DiyOmnitheca/Areas/Identity/Pages/Account/Manage/Payment.cshtml.cs
+++ DiyOmnitheca/Areas/Identity/Pages/Account/Manage/Payment.cshtml.cs
@@ -1,16 +1,14 @@
-namespace DiyOmnitheca.Areas.Identity.Pages.Account
+namespace DiyOmnitheca.Areas.Identity.Pages.Account.Manage
 {
     using DiyOmnitheca.Data;
     using DiyOmnitheca.Data.Models;
     using DiyOmnitheca.Infrastructure;
     using Microsoft.AspNetCore.Authorization;
-    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.RazorPages;
     using System.ComponentModel.DataAnnotations;
     using System.Linq;
     using System.Threading.Tasks;
-
     using static Data.DataConstants.Payment;
 
     [Authorize]
@@ -32,14 +30,16 @@ namespace DiyOmnitheca.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
-            [Display(Name = "Bank Name")]
+            [Display(Name = "New Bank Name")]
             [StringLength(BankNameMaxLength, MinimumLength = BankNameMinLength)]
             public string BankName { get; set; }
 
             [Required]
+            [Display(Name = "New IBAN")]
             [StringLength(IbanMaxLength, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = IbanMinLength)]
             public string Iban { get; set; }
 
+            [Display(Name = "Add/Remove Money")]
             public double Money { get; set; }
         }
 
