e7f92298c7ec417b7587c1d8e1eaaad85277fc3d
G.Zlatev <58149328+g-zlatev@users.noreply.github.com>
2021-08-24 20:33:40 +0300

    Added All Users View for Admins
    Works with Api visualized via JS and REST

---
+++ DiyOmnitheca/Areas/Admin/Controllers/UsersInfoController.cs
@@ -0,0 +1,12 @@
+﻿namespace DiyOmnitheca.Areas.Admin.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class UsersInfoController : AdminController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
