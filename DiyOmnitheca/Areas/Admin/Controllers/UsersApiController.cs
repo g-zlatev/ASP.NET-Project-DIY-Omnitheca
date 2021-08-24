86fbfda33fc45ab992104a46b8adf859dfd7cb32
G.Zlatev <58149328+g-zlatev@users.noreply.github.com>
2021-08-24 17:51:05 +0300

    implemented api/Users controller for Admins

---
+++ DiyOmnitheca/Areas/Admin/Controllers/UsersApiController.cs
@@ -0,0 +1,40 @@
+﻿namespace DiyOmnitheca.Areas.Admin.Controllers
+{
+    using DiyOmnitheca.Areas.Admin.Models;
+    using DiyOmnitheca.Data;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    [ApiController]
+    [Route("api/users")]
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class UsersApiController : ControllerBase
+    {
+
+        private readonly OmnithecaDbContext data;
+
+        public UsersApiController(OmnithecaDbContext data) 
+            => this.data = data;
+
+        [HttpGet]
+
+        public List<UserApiModel> All()
+        {
+            List<UserApiModel> users = this.data.Users.Select(u => new UserApiModel
+            {
+                Id = u.Id,
+                Email = u.Email,
+                FullName = u.FullName,
+                IsBorrower = this.data.Borrowers.Any(b => b.UserId == u.Id),
+                IsLender = this.data.Lenders.Any(l => l.UserId == u.Id),
+                HasPaymentInfo = this.data.PaymentInfos.Any(p => p.UserId == u.Id)
+            })
+                .ToList();
+
+            return users;
+        }
+    }
+}
---
+++ DiyOmnitheca/Areas/Admin/Models/UserApiModel.cs
@@ -0,0 +1,17 @@
+﻿namespace DiyOmnitheca.Areas.Admin.Models
+{
+    public class UserApiModel
+    {
+        public string Id { get; init; }
+
+        public string Email { get; init; }
+
+        public string FullName { get; init; }
+
+        public bool IsLender { get; init; }
+
+        public bool IsBorrower { get; init; }
+
+        public bool HasPaymentInfo { get; init; } 
+    }
+}
